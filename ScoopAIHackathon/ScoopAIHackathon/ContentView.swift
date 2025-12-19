25c6e0aa41c26d146fe80b1996f406f2697268b2
Velko <wlsgurrla716@naver.com>
2025-12-19 15:58:00 +0900

    🔧[Chore] - 프로젝트 생성

---
+++ ScoopAIHackathon/ScoopAIHackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoopAIHackathon
+//
+//  Created by 김진혁 on 12/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoopAIHackathon/ScoopAIHackathon/ScoopAIHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopAIHackathonApp.swift
+//  ScoopAIHackathon
+//
+//  Created by 김진혁 on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoopAIHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
